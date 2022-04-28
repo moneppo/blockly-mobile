86bcce0537ab23481793ff0ee56cb7cebd3b6e44
Glitch (glitch-blank-website) <none>
2022-04-28 03:19:08 +0000

    🏫👳 Checkpoint ./index.html:12019202/37 ./script.js:12019202/543

--- script.js
+++ script.js
@@ -5,4 +5,38 @@
 
 console.log("Hello, world!");
 
-let workspace = Blockly.inject(document.body, {});
+var toolbox = {
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "math_number"
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind": "block",
+      "type": "text_print"
+    },
+  ]
+}
+
+let workspace = Blockly.inject('n', {toolbox});
