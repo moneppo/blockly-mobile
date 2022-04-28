b55b53dc5811be749d9aa719bee4adc1258782e8
Glitch (glitch-blank-website) <none>
2022-04-28 03:07:57 +0000

    💑😘 Checkpoint ./index.html:12019202/500 ./script.js:12019202/115

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
+/* global Blockly */
+
 console.log("Hello, world!");
+
+let workspace = Blockly.inject(document.body, {});
