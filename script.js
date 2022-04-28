7e780c3cb28b91e4e1d5a92933ae0eb9c91012fc
Glitch (glitch-blank-website) <none>
2022-04-28 05:08:56 +0000

    💗💸 Checkpoint ./script.js:12019202/3509

--- script.js
+++ script.js
@@ -6,57 +6,59 @@
 console.log("Hello, world!");
 
 var toolbox = {
-  "kind": "flyoutToolbox",
-  "contents": [
+  kind: "flyoutToolbox",
+  contents: [
     {
-      "kind": "block",
-      "type": "controls_if"
+      kind: "block",
+      type: "controls_if",
     },
     {
-      "kind": "block",
-      "type": "controls_repeat_ext"
+      kind: "block",
+      type: "controls_repeat_ext",
     },
     {
-      "kind": "block",
-      "type": "logic_compare"
+      kind: "block",
+      type: "logic_compare",
     },
     {
-      "kind": "block",
-      "type": "math_number"
+      kind: "block",
+      type: "math_number",
     },
     {
-      "kind": "block",
-      "type": "math_arithmetic"
+      kind: "block",
+      type: "math_arithmetic",
     },
     {
-      "kind": "block",
-      "type": "text"
+      kind: "block",
+      type: "text",
     },
     {
-      "kind": "block",
-      "type": "text_print"
+      kind: "block",
+      type: "text_print",
     },
-  ]
-}
+  ],
+};
 
-class CustomConstantsProvider extends Blockly.zelos.ConstantProvider {
-  constructor() {
-    // Set up all of the constants from the base provider.
-    super();
-    this.TAB_HEIGHT = 30;
 
+
+class CustomRenderer extends Blockly.zelos.Renderer {
+  constructor(name) {
+    super(name);
+  }
+
+  makeRenderInfo_(block) {
+    const result = super.makeRenderInfo_(block);
+    console.log(block)
+    result.width = 300;
+    return result;
   }
 }
 
-class CustomRenderer extends Blockly.zelos.Renderer {
-  makeConstants() {
-    return new CustomConstantsProvider();
-  };
-}
+Blockly.blockRendering.register("custom_renderer", CustomRenderer);
 
-console.log(Blockly.zelos.Renderer)
-Blockly.blockRendering.register('custom_renderer', /*Blockly.zelos.Renderer*/CustomRenderer);
-
-let workspace = Blockly.inject('root', {toolbox, renderer:"custom_renderer"});
+let workspace = Blockly.inject("root", {
+  toolbox,
+  renderer: "custom_renderer",
+});
 
 //workspace.getFlyout().hide();
