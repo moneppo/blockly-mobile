529b0fb667ec3f840a24ae84f6ad614262b3c0bc
Glitch (glitch-blank-website) <none>
2022-04-28 21:48:20 +0000

    🙅🐠 Checkpoint ./script.js:12019202/1550

--- script.js
+++ script.js
@@ -39,23 +39,41 @@ var toolbox = {
   ],
 };
 
+class CustomRenderInfo extends Blockly.zelos.RenderInfo {
+  constructor(renderer, block) {
+    super(renderer, block);
+  }
+  
+  alignStatementRow_(row) {
+    const statementInput = row.getLastInput();
+    let currentWidth = row.width - statementInput.width;
+    let desiredWidth = this.statementEdge;
+    // Add padding before the statement input.
+    const missingSpace = desiredWidth - currentWidth;
+    if (missingSpace > 0) {
+      this.addAlignmentPadding_(row, missingSpace);
+    }
+    // Also widen the statement input to reach to the right side of the
+    // block. Note that this does not add padding.
+    currentWidth = row.width;
+    desiredWidth = this.getDesiredRowWidth_(row);
+    statementInput.width += (desiredWidth - currentWidth);
+    statementInput.height = Math.max(statementInput.height, row.height);
+    row.width += (desiredWidth - currentWidth);
+    row.widthWithConnectedBlocks =
+        Math.max(row.width, this.statementEdge + row.connectedBlockWidths);
+    
+    row.width = this.block_.workspace.getWidth();
+  }
+}
+
 class CustomRenderer extends Blockly.zelos.Renderer {
   constructor(name) {
     super(name);
   }
 
   makeRenderInfo_(block) {
-    const result = super.makeRenderInfo_(block);
-    
-    console.log(result.statementEdge, result.width)
-    
-    // Resize statement blocks to fill screen horizontally
-    if (block.outputConnection === null) {
-      result.width = 2; //block.workspace.getWidth();
-      result.statementEdge = block.workspace.getWidth();
-    }
-    
-    return result;
+    return new CustomRenderInfo(this, block);
   }
 }
 
