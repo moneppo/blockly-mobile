70a54653433c49fae41ecfd0fd3406a372f7eb5d
Glitch (glitch-blank-website) <none>
2022-04-29 23:47:23 +0000

    😘🖼 Checkpoint ./script.js:12019202/115

--- script.js
+++ script.js
@@ -39,41 +39,40 @@ var toolbox = {
   ],
 };
 
-class CustomRenderInfo extends Blockly.zelos.RenderInfo {
-  constructor(renderer, block) {
-    super(renderer, block);
-  }
+// class CustomRenderInfo extends Blockly.zelos.RenderInfo {
+//   constructor(renderer, block) {
+//     super(renderer, block);
+//   }
   
-  getDesiredRowWidth_(row) {
-   // this.width = this.block_.workspace.getWidth();
-    if (row.hasStatement) {
-      const rightCornerWidth = this.constants_.INSIDE_CORNERS.rightWidth || 0;
-      return this.width - this.startX - rightCornerWidth;
-    }
+//   getDesiredRowWidth_(row) {
+//    // this.width = this.block_.workspace.getWidth();
+//     if (row.hasStatement) {
+//       const rightCornerWidth = this.constants_.INSIDE_CORNERS.rightWidth || 0;
+//       return this.width - this.startX - rightCornerWidth;
+//     }
     
-    return this.width - this.startX; //super.getDesiredRowWidth_(row);
-  }
-}
+//     return this.width - this.startX; //super.getDesiredRowWidth_(row);
+//   }
+// }
 
-class CustomRenderer extends Blockly.zelos.Renderer {
-  constructor(name) {
-    super(name);
-  }
+// class CustomRenderer extends Blockly.zelos.Renderer {
+//   constructor(name) {
+//     super(name);
+//   }
 
-  makeRenderInfo_(block) {
-    const result = new CustomRenderInfo(this, block);
-    if (!block.isInFlyout)
-      console.log(result);
-    result.width = block.workspace.getWidth() - result.statementEdge;
-    return result;
-  }
-}
+//   makeRenderInfo_(block) {
+//     const result = new CustomRenderInfo(this, block);
+//     if (!block.isInFlyout)
+//     result.width = block.workspace.getWidth() - result.statementEdge;
+//     return result;
+//   }
+// }
 
-Blockly.blockRendering.register("custom_renderer", CustomRenderer);
+// Blockly.blockRendering.register("custom_renderer", CustomRenderer);
 
 let workspace = Blockly.inject("root", {
   toolbox,
-  renderer: "custom_renderer",
+  renderer: "zelos", //"custom_renderer",
 });
 
-//workspace.getFlyout().hide();
+workspace.getFlyout().hide();
