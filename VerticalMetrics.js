f5d5d84e81faa8cd7e381fa447e726635f2ea8c5
Glitch (glitch-blank-website) <none>
2022-05-03 18:37:45 +0000

    👔🐊 Checkpoint ./VerticalMetrics.js:12019202/4394 ./Workspace.js:12019202/4

--- VerticalMetrics.js
+++ VerticalMetrics.js
@@ -5,23 +5,43 @@ export default class extends Blockly.MetricsManager {
     super(workspace);
   }
 
-  getAbsoluteMetrics() {
-    return {left: 0, top: 0}
+  hasFixedEdges() {
+    return true;
   }
-  
+
+  getAbsoluteMetrics() {
+    return { left: 0, top: 0 };
+  }
+
   getScrollMetrics(
-      getWorkspaceCoordinates = undefined,
-      cachedViewMetrics = undefined,
-      cachedContentMetrics = undefined) {
-    
-    
+    getWorkspaceCoordinates = undefined,
+    cachedViewMetrics = undefined,
+    cachedContentMetrics = undefined
+  ) {
     const scrollMetrics = super.getScrollMetrics(
-        getWorkspaceCoordinates, cachedViewMetrics, cachedContentMetrics);
-    const contentMetrics = cachedContentMetrics ||
-        this.getContentMetrics(getWorkspaceCoordinates);
-    const viewMetrics = cachedViewMetrics ||
-        this.getViewMetrics(getWorkspaceCoordinates);
+      getWorkspaceCoordinates,
+      cachedViewMetrics,
+      cachedContentMetrics
+    );
+    const contentMetrics =
+      cachedContentMetrics || this.getContentMetrics(getWorkspaceCoordinates);
+    const viewMetrics =
+      cachedViewMetrics || this.getViewMetrics(getWorkspaceCoordinates);
 
     return scrollMetrics;
+  }
+  
+
+  getComputedFixedEdges_(opt_viewMetrics) {
+    const vScrollEnabled = this.workspace_.isMovableVertically();
+    const viewMetrics = opt_viewMetrics || this.getViewMetrics(false);
+
+    const edges = {};
+
+    edges.top = 0;
+
+    edges.left = viewMetrics.left;
+    edges.right = viewMetrics.left + viewMetrics.width;
+    return edges;
   }
 }
