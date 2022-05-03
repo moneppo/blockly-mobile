871c93b1e3a06b75f5735f3281d5f26046a29a5b
Glitch (glitch-blank-website) <none>
2022-05-03 15:40:38 +0000

    🎙🏗 Checkpoint ./toolbox.js:12019202/3911

--- toolbox.js
+++ toolbox.js
@@ -1,13 +1,5 @@
 /* global Blockly */
 
-export const blocks = {
-  play_sample: {
-    message0: "play sample",
-    nextStatement: null,
-    colour: 230,
-  },
-};
-
 Blockly.Blocks["top"] = {
   init: function () {
     this.jsonInit({
@@ -24,30 +16,45 @@ Blockly.Blocks["top"] = {
   },
 };
 
-Object.entries(blocks).forEach((name, block) => {
-  Blockly.Blocks["play_sample"] = {
-    init: function () {
-      this.jsonInit({
-        type: "top",
-        message0: "when started",
-        nextStatement: null,
-        colour: 230,
-        tooltip: "",
-        helpUrl: "",
-      });
-      this.setEditable(false);
-      this.setMovable(false);
-      this.setDeletable(false);
-    },
-  };
-});
+Blockly.Blocks["play_sample"] = {
+  init: function () {
+    this.jsonInit({
+      type: "play_sample",
+      message0: "%1 play sample %2",
+      args0: [
+        {
+          type: "field_image",
+          src: "https://www.gstatic.com/codesite/ph/images/star_on.gif",
+          width: 36,
+          height: 36,
+          alt: "*",
+          flipRtl: false,
+        },
+        {
+          type: "field_dropdown",
+          name: "NAME",
+          options: [
+            ["option", "OPTIONNAME"],
+            ["option", "OPTIONNAME"],
+            ["option", "OPTIONNAME"],
+          ],
+        },
+      ],
+      previousStatement: null,
+      nextStatement: null,
+      colour: 230,
+      tooltip: "",
+      helpUrl: "",
+    });
+  },
+};
 
-export const toolbox = {
+export default {
   kind: "flyoutToolbox",
   contents: [
     {
       kind: "block",
-      type: "text_print",
+      type: "play_sound",
       icon: "bi bi-music-note",
       display_name: "Play Sound",
     },
