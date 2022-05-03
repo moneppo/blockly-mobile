d6b23ac3faa6217a332a9ab3f1c404467c6a5b82
Glitch (glitch-blank-website) <none>
2022-05-03 16:49:34 +0000

    🗺📠 Checkpoint ./VerticalMetrics.js:12019202/872

--- VerticalMetrics.js
+++ VerticalMetrics.js
@@ -8,4 +8,20 @@ export default class extends Blockly.MetricsManager {
   getAbsoluteMetrics() {
     return {left: 0, top: 0}
   }
+  
+  getScrollMetrics(
+      getWorkspaceCoordinates = undefined,
+      cachedViewMetrics = undefined,
+      cachedContentMetrics = undefined) {
+    
+    
+    const scrollMetrics = super.getScrollMetrics(
+        getWorkspaceCoordinates, cachedViewMetrics, cachedContentMetrics);
+    const contentMetrics = cachedContentMetrics ||
+        this.getContentMetrics(getWorkspaceCoordinates);
+    const viewMetrics = cachedViewMetrics ||
+        this.getViewMetrics(getWorkspaceCoordinates);
+
+    return scrollMetrics;
+  }
 }
