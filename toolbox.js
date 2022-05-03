6d94cf9337de100de0aaa6ff7ee4527937cf06eb
Glitch (glitch-blank-website) <none>
2022-05-03 15:28:49 +0000

    🚁🏓 Checkpoint ./toolbox.js:12019202/4884

--- toolbox.js
+++ toolbox.js
@@ -1,5 +1,13 @@
 /* global Blockly */
 
+export const blocks = {
+  play_sample: {
+    message0: "play sample",
+    nextStatement: null,
+    colour: 230,
+  },
+};
+
 Blockly.Blocks["top"] = {
   init: function () {
     this.jsonInit({
@@ -16,14 +24,32 @@ Blockly.Blocks["top"] = {
   },
 };
 
-export default {
+Object.entries(blocks).forEach((name, block) => {
+  Blockly.Blocks["play_sample"] = {
+    init: function () {
+      this.jsonInit({
+        type: "top",
+        message0: "when started",
+        nextStatement: null,
+        colour: 230,
+        tooltip: "",
+        helpUrl: "",
+      });
+      this.setEditable(false);
+      this.setMovable(false);
+      this.setDeletable(false);
+    },
+  };
+});
+
+export const toolbox = {
   kind: "flyoutToolbox",
   contents: [
     {
       kind: "block",
       type: "text_print",
       icon: "bi bi-music-note",
-      display_name: "Play Sound"
+      display_name: "Play Sound",
     },
   ],
 };
