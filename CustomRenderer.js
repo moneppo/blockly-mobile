b3b15957a30263912da33e2ccf35b374da848157
Glitch (glitch-blank-website) <none>
2022-05-04 00:19:38 +0000

    👩‍👩‍👦🚊 Checkpoint ./CustomRenderer.js:12019202/5994

--- CustomRenderer.js
+++ CustomRenderer.js
@@ -1,35 +1,35 @@
 /* global Blockly */
 
-
 class CustomRenderInfo extends Blockly.zelos.RenderInfo {
   constructor(renderer, block) {
     super(renderer, block);
   }
-  
 
-//   getDesiredRowWidth_(row) {
-//    // this.width = this.block_.workspace.getWidth();
-//     if (row.hasStatement) {
-//       const rightCornerWidth = this.constants_.INSIDE_CORNERS.rightWidth || 0;
-//       return this.width - this.startX - rightCornerWidth;
-//     }
+  finalize_() {
+        super.finalize_();
+    console.log("FOR:", this.block_)
+    for (let i = 0; i < this.rows.length; i++) {
+      const row = this.rows[i];
+      if (!Blockly.blockRendering.Types.isInputRow(row)) {
+        continue;
+      }
+      const firstElem = row.elements[1];
+      const lastElem = row.elements[row.elements.length - 2];
+      console.log(firstElem, lastElem);
+      firstElem.width = 1000;
 
-//     return this.width - this.startX; //super.getDesiredRowWidth_(row);
-//   }
- }
+    }
+  }
+}
 
 class CustomRenderer extends Blockly.zelos.Renderer {
-
   constructor(name) {
     super(name);
   }
 
-//   makeRenderInfo_(block) {
-//     const result = new CustomRenderInfo(this, block);
-//     if (!block.isInFlyout)
-//     result.width = block.workspace.getWidth() - result.statementEdge;
-//     return result;
-//   }
- }
+  makeRenderInfo_(block) {
+    return new CustomRenderInfo(this, block);
+  }
+}
 
 Blockly.blockRendering.register("custom_renderer", CustomRenderer);
