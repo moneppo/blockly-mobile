606475acd6981ed528df4ebe1c1036033ae8d14a
Glitch (glitch-blank-website) <none>
2022-05-04 23:42:36 +0000

    👛🏗 Checkpoint ./index.html:12019202/120 ./Designer.js:12019202/360

---
+++ Designer.js
@@ -0,0 +1,11 @@
+/* global fabric */
+
+import { h } from "https://unpkg.com/preact@latest?module";
+import { useState } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
+import htm from "https://unpkg.com/htm?module";
+
+const html = htm.bind(h);
+
+export default () => {
+  
+}
