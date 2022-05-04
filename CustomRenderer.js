e698c10e071fe2886dd2ad723196be827d84f8e7
Glitch (glitch-blank-website) <none>
2022-05-04 01:40:48 +0000

    🍽📹 Checkpoint ./CustomRenderer.js:12019202/2477

--- CustomRenderer.js
+++ CustomRenderer.js
@@ -4,29 +4,18 @@ class CustomRenderInfo extends Blockly.zelos.RenderInfo {
   constructor(renderer, block) {
     super(renderer, block);
   }
-  
+
   addElemSpacing_() {
     super.addElemSpacing_();
-     for (let i = 0, row; (row = this.rows[i]); i++) {
+    for (let i = 0, row; (row = this.rows[i]); i++) {
       // No spacing needed before the corner on the top row or the bottom row.
-      if (row.startsWithElemSpacer()) {
-        
+      if (row.startsWithElemSpacer() && row.type ) {
+        row.elements[0].width += 20;
       }
-  }
 
-  finalize_() {
-    super.finalize_();
-    console.log("FOR:", this.block_, this);
-    for (let i = 0; i < this.rows.length; i++) {
-      const row = this.rows[i];
-      const spacing = 300 / row.elements.filter(e => Blockly.blockRendering.Types.isSpacer(e)).length;
-      for (let s = 0; s < row.elements.length; s++) {
-        const element = row.elements[s];
-        if (Blockly.blockRendering.Types.isSpacer(element)) {
-          element.width += spacing;
-        }
+      if (row.endsWithElemSpacer()) {
+        row.elements[row.elements.length - 1].width += 20;
       }
-      console.log(row);
     }
   }
 }
