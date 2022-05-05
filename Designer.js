bc08cfa14d8db9595588a4a771bfdbfb57285885
Glitch (glitch-blank-website) <none>
2022-05-05 00:17:42 +0000

    ⛳️😇 Checkpoint ./Designer.js:12019202/87

--- Designer.js
+++ Designer.js
@@ -1,7 +1,7 @@
 /* global fabric */
 
 import { h, createRef } from "https://unpkg.com/preact@latest?module";
-import { useState, useMemo } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
+import { useState, useMemo, useEffect } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
 import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
@@ -11,6 +11,8 @@ export default () => {
   const canvas = useMemo(() => {
     return new fabric.Canvas(ref.current);
   }, [ref]);
+  
+
   
   return html`<canvas ref=${ref}/>`;
 }
