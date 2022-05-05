41a5595cbdd740955972fc4ba52514b6badfe8eb
Glitch (glitch-blank-website) <none>
2022-05-05 04:37:16 +0000

    💽🚿 Checkpoint ./Designer.js:12019202/7162

--- Designer.js
+++ Designer.js
@@ -4,52 +4,49 @@ import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
-const Rect = () => {
-    const ref = createRef();
- 
-    const startDrag = (event) => {
-      const svg = ref.current.ownerSVGElement;
-      event.preventDefault();
-      let point = svg.createSVGPoint();
-      point.x = event.clientX;
-      point.y = event.clientY;
-      point = point.matrixTransform(svg.getScreenCTM().inverse());
-    this.setState({dragOffset: {
-      x: point.x - this.state.rect.x,
-      y: point.y - this.state.rect.y
-    }});
-    
+const Rect = ({x, y, update}) => {
+  const ref = createRef();
+  const [dragOffset, setDragOffset] = useState();
+
+  const startDrag = (event) => {
+    const svg = ref.current.ownerSVGElement;
+    event.preventDefault();
+    let point = DOMPoint(event.clientX, event.clientY);
+    point = point.matrixTransform(svg.getScreenCTM().inverse());
+    setDragOffset({ x: point.x - x, y: point.y - y });
+
     const mousemove = (event) => {
       event.preventDefault();
       point.x = event.clientX;
       point.y = event.clientY;
-      let cursor = point.matrixTransform(this.svg.getScreenCTM().inverse());
-      this.setState({rect: {
-        x: cursor.x - this.state.dragOffset.x,
-        y: cursor.y - this.state.dragOffset.y
-      }});
+      let cursor = point.matrixTransform(svg.getScreenCTM().inverse());
+      update(
+        cursor.x - this.state.dragOffset.x,
+        cursor.y - this.state.dragOffset.y);
     };
-    
+
     const mouseup = (event) => {
       document.removeEventListener("mousemove", mousemove);
       document.removeEventListener("mouseup", mouseup);
     };
-    
+
     document.addEventListener("mousemove", mousemove);
     document.addEventListener("mouseup", mouseup);
-  }
-}
+  };
 
-  
-    return html`
-      <svg viewBox="0 0 100 100">
-        <rect
-          x={this.state.rect.x}
-          y={this.state.rect.y}
-          width="20"
-          height="20"
-          ref=${ref}
-          onMouseDown=${startDrag}
-        />
-      </svg>`;
-  
+  return html`
+    <rect
+      x=${x}
+      y=${y}
+      width="20"
+      height="20"
+      ref=${ref}
+      onMouseDown=${startDrag}
+    />`;
+};
+
+export default () => {
+  return html`<svg viewBox="0 0 100 100">
+    <${}
+  `
+}
