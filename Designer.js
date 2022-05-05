ebf2e113adfc96356d560dbee02024db10634d90
Glitch (glitch-blank-website) <none>
2022-05-05 00:41:36 +0000

    👵⚽️ Checkpoint ./Designer.js:12019202/3140

--- Designer.js
+++ Designer.js
@@ -1,30 +1,50 @@
 /* global fabric */
 
 import { h, createRef } from "https://unpkg.com/preact@latest?module";
-import { useState, useMemo, useEffect } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
+import {
+  useState,
+  useMemo,
+  useEffect,
+} from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
 import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
 function resizeCanvas(canvas) {
-    const { width, height } = canvas.getBoundingClientRect()
-    
-    if (canvas.width !== width || canvas.height !== height) {
-      const { devicePixelRatio:ratio=1 } = window
-      const context = canvas.getContext('2d')
-      canvas.width = width*ratio
-      canvas.height = height*ratio
-      context.scale(ratio, ratio)
-    }
+  console.log("resize");
+  const { width, height } = canvas.getBoundingClientRect();
+
+  if (canvas.width !== width || canvas.height !== height) {
+    const { devicePixelRatio: ratio = 1 } = window;
+    const context = canvas.getContext("2d");
+    canvas.width = width * ratio;
+    canvas.height = height * ratio;
+    context.scale(ratio, ratio);
   }
+}
 
 export default () => {
   const ref = createRef();
   if (ref.current) resizeCanvas(ref.current);
-  
-  const canvas = useMemo(() => {
-    return new fabric.Canvas(ref.current);
+
+  const c = useMemo(() => {
+    let canvas = new fabric.Canvas(ref.current);
+    canvas.setBackgroundColor("rgba(255, 73, 64, 0.6)");
+    var rect = new fabric.Rect({
+      left: 100,
+      top: 50,
+      fill: "yellow",
+      width: 200,
+      height: 100,
+      objectCaching: false,
+      stroke: "lightgreen",
+      strokeWidth: 4,
+    });
+
+    canvas.add(rect);
+    canvas.setActiveObject(rect);
+    return canvas;
   }, [ref]);
-  
-  return html`<canvas ref=${ref} />`;
-}
+
+  return html`<canvas ref=${ref} width="400" height="300" />`;
+};
