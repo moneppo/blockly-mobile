b922669b161f467d90c0f4be2a8ed0d752568ee9
Glitch (glitch-blank-website) <none>
2022-05-05 05:12:28 +0000

    ✋🏂 Checkpoint ./Designer.js:12019202/1597

--- Designer.js
+++ Designer.js
@@ -4,12 +4,40 @@ import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
-const Button = ({ selected, x, y, update }) => {
+const Rotator = ({width, height, rotation, update}) => {
+   const ref = createRef();
+  const [offset, setOffset] = useState(0);
+
+  const startRotate = (event) => {
+    const svg = ref.current.ownerSVGElement;
+    event.preventDefault();
+    let point = new DOMPoint(, event.clientY);
+    point = point.matrixTransform(svg.getScreenCTM().inverse());
+    setDragOffset(event.clientX);
+
+    const mousemove = (event) => {
+      event.preventDefault();
+      point.x = event.clientX;
+      point.y = event.clientY;
+      let cursor = point.matrixTransform(svg.getScreenCTM().inverse());
+      update(cursor.x - dragOffset.x, cursor.y - dragOffset.y);
+    };
+
+    const mouseup = () => {
+      document.removeEventListener("mousemove", mousemove);
+      document.removeEventListener("mouseup", mouseup);
+    };
+
+  }
+  return html`<ellipse ref=${ref} cx="20" cy="20" rx="3" ry="3"/>`;
+  
+}
+
+const Button = ({ selected, x, y, update, select }) => {
   const ref = createRef();
   const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
 
   const startDrag = (event) => {
-    console.log("hai");
     const svg = ref.current.ownerSVGElement;
     event.preventDefault();
     let point = new DOMPoint(event.clientX, event.clientY);
@@ -17,7 +45,6 @@ const Button = ({ selected, x, y, update }) => {
     setDragOffset({ x: point.x - x, y: point.y - y });
 
     const mousemove = (event) => {
-      console.log("huh?")
       event.preventDefault();
       point.x = event.clientX;
       point.y = event.clientY;
@@ -35,19 +62,20 @@ const Button = ({ selected, x, y, update }) => {
   };
 
   return html` <g ref=${ref} transform="translate(${x} ${y})">
-    <rect width="20" height="20" onMouseDown=${startDrag} />
-     <polyline points="0,100 50,25 50,75 100,0" fill="gray"/>
+    <rect width="20" height="20" onMouseDown=${selected ? startDrag : select} />
+    ${selected && html`<${Rotator}/>`}
   </g>`;
 };
 
 export default () => {
   const [pos, setPos] = useState([0, 0]);
+  const [selected, setSelected] = useState(false);
 
   const update = (x, y) => {
     setPos([x, y]);
   };
 
   return html`<svg viewBox="0 0 100 100">
-    <${Button} x=${pos[0]} y=${pos[1]} update=${update} />
+    <${Button} selected=${selected} x=${pos[0]} y=${pos[1]} update=${update} select=${setSelected}/>
   </svg> `;
 };
