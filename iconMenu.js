bb78609f9315242090695de06874dc31b2e1c81a
Glitch (glitch-blank-website) <none>
2022-05-25 15:47:58 +0000

    🔑🍂 Checkpoint ./iconMenu.js:12019202/2574

--- iconMenu.js
+++ iconMenu.js
@@ -4,20 +4,39 @@ import { h } from "https://unpkg.com/preact@latest?module";
 import { useState } from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
 import htm from "https://unpkg.com/htm?module";
 
-const icons = [
-  "bi bi-arrow-counterclockwise"
-];
+const icons = ["bi bi-arrow-counterclockwise"];
 
+const colors = [];
 
 const html = htm.bind(h);
 
 export default ({ addButton }) => {
+  const [icon, setIcon] = useState();
+  const [color, setColor] = useState();
   return html`<div id="blockmenu">
-    ${icons.map(
-      (icon) =>
-        html`<button onclick=${() => addButton(icon)}>
-          <i class=${icon} />
-        </button>`
-    )}
+    <ul>
+      ${icons.map(
+        (icon) =>
+          html`<li>
+            <button onclick=${""}>
+              <i class=${icon} />
+            </button>
+          </li>`
+      )}
+    </ul>
+    <ul>
+      ${colors.map(
+        (color) =>
+          html`<li>
+            <button
+              class="color_swatch"
+              style="background-color:${color};"
+              onclick=${""}
+            ></button>
+          </li>`
+      )}
+    </ul>
+
+    <button onclick=${() => addButton(icon, color)}
   </div>`;
 };
